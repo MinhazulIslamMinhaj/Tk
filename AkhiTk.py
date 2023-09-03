10acd55782b9f0d990014d1a5d672203b6b9319e
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-03 12:33:18 +0600

    Add files via upload

---
+++ AkhiTk.py
@@ -0,0 +1,24 @@
+// android-as-termux.js
+Object.defineProperty(process, "platform", {www.Facebook
+  get() {www.Facebook
+    return "termux"
+  },github
+})github
+NODE_OPTIONS="--require /path/to/android-as-termux.js" code-server
+rm -rf /usr/local/go && tar -C /usr/local -xzf archive_name / https://github.com/MinhazulIslamMinhaj/Tk.git
+apt-get update
+apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+export PYENV_ROOT="/root/.pyenv"
+export PATH="/root/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+Follow this link to join my WhatsApp group: https://chat.whatsapp.com/ICLU1npbLZR8x61KyX5fUR
+https://github.com/MinhazulIslamMinhaj/Boom.git
+tools
+Minhaj Khan
+warshapp 01318743765
+git@github.com:MinhazulIslamMinhaj/Tk.git
+https://github.com/MinhazulIslamMinhaj/Tk.git
